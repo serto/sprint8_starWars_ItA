654abf78b3f1ee59c21bdc3057e6e60aa0882080
Sertorio Morales <sertomorales@gmail.com>
2022-03-17 14:22:01 +0100

    solucio ex 7

--- src/application/routes.js
+++ src/application/routes.js
@@ -1,23 +1,43 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import React, {useState, useEffect} from "react";
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Home from '../pages/home';
 import Detail from '../pages/detail';
 import List from '../pages/list';
 import LoginSign from '../pages/loginSign';
 import ErrorPage from '../pages/errorPage';
 
+
 const RoutesApp = () => {
+
+  const logged = JSON.parse(localStorage.getItem("userSWLogged"));
+  const [show, setShow] = useState(logged ? true : false);
+
+  useEffect(() => { 
+
+    const loggedLS = JSON.parse(localStorage.getItem("userSWLogged"));
+    setShow(loggedLS);
+  }, [show]);
+
   return (
     <BrowserRouter>
       <Routes>   
         <Route path="/" element={<Home/>} />
-        <Route path="/list" element={<List/>} />
-        <Route path="/ship/:id" element={<Detail/>} />
+        <Route
+          path="/list"
+          element={ show ? ( <List /> ) : ( <Navigate replace to="/loginSignUp" /> ) }
+        />
+        <Route
+          path="/ship/:id"
+          element={ show ? ( <Detail /> ) : ( <Navigate replace to="/loginSignUp" /> ) }
+        />
         <Route path="/loginSignUp" element={<LoginSign/>} />
         <Route path="*" element={<ErrorPage /> } />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 
 export default RoutesApp;
--- src/components/listShip/listShip.js
+++ src/components/listShip/listShip.js
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { ListShipStyle } from './listShip.style';
 import { Link } from 'react-router-dom';
 
